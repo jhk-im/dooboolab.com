47d7310c7379270466a9dca0c50ae0bec884c7ec
ll0301 <wanted0301@gmail.com>
2020-07-26 23:27:34 +0900

    add images.ts

---
+++ src/utils/images.ts
@@ -0,0 +1,7 @@
+import contactBackground from '../../assets/images/contact-background.png';
+import introBackground from '../../assets/images/intro-background.png';
+import visionBackground from '../../assets/images/vision-background.png';
+
+export const CONTACT_BACKGROUND = contactBackground;
+export const INTRO_BACKGROUND = introBackground;
+export const VISION_BACKGROUND = visionBackground;
